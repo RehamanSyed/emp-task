2d9db50e892b9ffe39aeaadd81fea7013a2cc02d
RehamanSyed <rehaman.syed1990@gmail.com>
2023-11-24 21:50:54 +0300

    updated code

--- src/pages/api/upload.js
+++ src/pages/api/upload.js
@@ -1,5 +1,4 @@
-// pages/api/upload.js
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
 import multer from "multer";
 
@@ -14,16 +13,17 @@ export const config = {
 
 export default async function handler(req, res) {
   try {
-    await upload.single("profile_image")(req, res, function (err) {
+    await upload.single("profile_image")(req, res, async function (err) {
       if (err) {
         return res.status(400).json({ message: "Failed to upload image" });
       }
-      const imageBuffer = req.file.buffer;
-      const fileName = `${req.file.originalname}`;
-      const filePath = path.join("public", fileName);
 
-      // Save the image buffer to the server
-      fs.writeFileSync(filePath, imageBuffer);
+      const imageBuffer = req.file.buffer;
+      const fileName = req.file.originalname;
+      const filePath = path.join(process.cwd(), "public", fileName);
+
+      // Save the image buffer to the server asynchronously
+      await fs.writeFile(filePath, imageBuffer);
 
       return res.status(200).json({
         success: true,
